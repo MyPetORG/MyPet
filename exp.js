b51e7ec4ac22261db7074797d39600cf4ad2798a
Keyle <keyle@t-online.de>
2011-07-21 21:31:43 +0200

    added comments to the example EXP-System Level Script

--- exp.js
+++ exp.js
@@ -1,12 +1,34 @@
+/*
+	#######################################
+	###									###
+	### MyWolf EXP-System Level Script 	###
+	### 		by Keyle 				###
+	###									###
+	#######################################
+	
+		Usable variables:
+			lvl 	-> write the return level in this variable
+			EXP		-> EXP the wolf has
+			factor	-> EXP factor from config
+			name	-> wolf's name
+			player	-> name of the owner
+			maxhp	-> actual maxHP value the wolf has
+			lives	-> lives the wolf ha
+			
+*/
+
+//declare variables
 var lvl;
 var EXP;
 var factor;
 
+//example start
 var tmplvl = 1;
 
 for (i = factor * factor; i <= EXP; i = i * factor)
 {
 	tmplvl++;
 }
+//example end
 
-lvl = tmplvl;
+lvl = tmplvl; // set return value
