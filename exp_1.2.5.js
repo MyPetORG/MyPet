067ead62877fd3c945ea1e5ae3c25d0d8c617099
xXKeyleXx <keyle@t-online.de>
2012-08-08 13:42:08 +0200

    renamed exp script and added new script for MC 1.3.1

---
+++ exp_1.2.5.js
@@ -0,0 +1,45 @@
+/*
+	#####################################
+	###                               ###
+	### MyPet Exp-System Level Script ###
+	###         by Keyle              ###
+	###       MC 1.2.5 script         ###
+	###  rename this file to exp.js   ###
+	###        befor using it         ###
+	###                               ###
+	#####################################
+	
+		required return varibles:
+		    lvl         -> write the return level in this variable
+			requiredExp -> write the require Exp for the next level in this variable
+			currentExp  -> write the current Exp for this level in this variable
+
+
+		Usable variables:
+		    type    -> Mobtype of the pet
+			exp		-> Exp the pet has
+			name	-> The name of the pet
+			player	-> The name of the owner
+*/
+
+//   |---------------|
+//   | example start |
+//   |---------------|
+
+    var tmpExp = exp;
+    var tmpLvl = 0;
+//  Minecraft:E = 7 + roundDown (  n    * 3.5)
+    while (tmpExp >= 7 + Math.floor(tmpLvl * 3.5))
+    {
+        tmpExp -= 7 + Math.floor(tmpLvl * 3.5);
+        tmpLvl++;
+    }
+
+//  set return values
+    lvl = tmpLvl+1;
+    requiredExp = 7 + Math.floor(tmpLvl * 3.5);
+    currentExp = tmpExp;
+
+//   |---------------|
+//   |  example end  |
+//   |---------------|
