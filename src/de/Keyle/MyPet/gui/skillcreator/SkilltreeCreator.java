473ccac1c5305b80784b8027fe796ffbbe68915f
xXKeyleXx <keyle@t-online.de>
2012-12-13 16:04:10 +0100

    moved tooltip from source to form

--- src/de/Keyle/MyPet/gui/skillcreator/SkilltreeCreator.java
+++ src/de/Keyle/MyPet/gui/skillcreator/SkilltreeCreator.java
@@ -377,6 +377,5 @@ public class SkilltreeCreator
     {
         skillTreeListModel = new DefaultListModel();
         skilltreeList = new JList(skillTreeListModel);
-        skilltreeList.setToolTipText("Doubleclick an item to edit a skilltree.");
     }
 }
