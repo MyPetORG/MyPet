246acb0295b94bcd469cb1cb9a3fbfe0b22027c4
xXKeyleXx <keyle@t-online.de>
2013-01-09 22:44:36 +0100

    added EntitySize test

---
+++ src/Test/de/Keyle/MyPet/test/entity/EntitySizeTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2011-2013 Keyle
+ *
+ * This file is part of MyPet
+ *
+ * MyPet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyPet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MyPet. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.Keyle.MyPet.test.entity;
+
+import de.Keyle.MyPet.entity.EntitySize;
+import de.Keyle.MyPet.entity.types.EntityMyPet;
+import de.Keyle.MyPet.entity.types.MyPetType;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class EntitySizeTest
+{
+    @Test
+    public void testEntitySizes()
+    {
+        for (MyPetType petType : MyPetType.values())
+        {
+            Class<? extends EntityMyPet> entityClass = petType.getEntityClass();
+            EntitySize es = entityClass.getAnnotation(EntitySize.class);
+            assertNotNull(es);
+            assertTrue(es.height() > 0);
+            assertTrue(es.width() > 0);
+        }
+    }
+}
