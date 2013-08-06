99afd68b0d0030c80d9a58806c944bb6fccde4dd
xXKeyleXx <keyle@t-online.de>
2013-08-06 18:54:24 +0200

    sort Skillnames alphabetically

--- src/main/java/de/Keyle/MyPet/gui/skilltreecreator/LevelCreator.java
+++ src/main/java/de/Keyle/MyPet/gui/skilltreecreator/LevelCreator.java
@@ -48,6 +48,7 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
@@ -99,6 +100,7 @@ public class LevelCreator
                     skillNames[skillCounter++] = sn.value();
                 }
             }
+            Arrays.sort(skillNames);
         }
 
         addLevelButton.addActionListener(new ActionListener()
