611b300425170c42bb804bc837c8c4d0814bc30f
xXKeyleXx <keyle@t-online.de>
2013-09-18 20:37:35 +0200

    pets will sneak when the owner is sneaking

--- src/main/java/de/Keyle/MyPet/entity/types/EntityMyPet.java
+++ src/main/java/de/Keyle/MyPet/entity/types/EntityMyPet.java
@@ -339,6 +339,9 @@ public abstract class EntityMyPet extends EntityCreature implements IMonster {
     }
 
     public void onLivingUpdate() {
+        if (getOwner().getPlayer().isSneaking() != isSneaking()) {
+            this.setSneaking(!isSneaking());
+        }
         if (!this.isInvisible() && Configuration.DONATOR_EFFECT && getOwner().isDonator() && donatorParticleCounter-- <= 0) {
             donatorParticleCounter = 20 + aC().nextInt(10);
             BukkitUtil.playParticleEffect(this.getBukkitEntity().getLocation().add(0, 1, 0), "happyVillager", 0.4F, 0.4F, 0.4F, 0.4F, 5, 10);
