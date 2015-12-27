e99791f7f2b7c6556708cd0bcd8b5ad9f143c329
xXKeyleXx <keyle@t-online.de>
2015-12-27 13:43:09 +0100

    update copyright header

--- src/main/java/de/Keyle/MyPet/repository/types/NbtRepository.java
+++ src/main/java/de/Keyle/MyPet/repository/types/NbtRepository.java
@@ -1,15 +1,15 @@
 /*
- * This file is part of MyPet-1.8
+ * This file is part of MyPet
  *
- * Copyright (C) 2011-2015 Keyle
- * MyPet-1.8 is licensed under the GNU Lesser General Public License.
+ * Copyright (C) 2011-2016 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
  *
- * MyPet-1.8 is free software: you can redistribute it and/or modify
+ * MyPet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MyPet-1.8 is distributed in the hope that it will be useful,
+ * MyPet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
