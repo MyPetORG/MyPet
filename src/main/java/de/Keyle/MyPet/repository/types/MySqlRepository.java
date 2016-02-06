5feb280d87d5393ba3679015beb931d315ef6b28
xXKeyleXx <keyle@t-online.de>
2016-02-06 11:45:30 +0100

    fix owner of pet not beeing found after switching from offline to online mode (MySQL)

--- src/main/java/de/Keyle/MyPet/repository/types/MySqlRepository.java
+++ src/main/java/de/Keyle/MyPet/repository/types/MySqlRepository.java
@@ -809,7 +809,7 @@ public class MySqlRepository implements Repository {
                             MyPetLogger.write(ChatColor.RED + "Can't get UUID for \"" + playerName + "\"! Pets may not be loaded for this player!");
                             return null;
                         } else {
-                            petPlayer = new OnlineMyPetPlayer(fetchedUUIDs.get(playerName));
+                            petPlayer = new OnlineMyPetPlayer(internalUUID, fetchedUUIDs.get(playerName));
                             ((OnlineMyPetPlayer) petPlayer).setLastKnownName(playerName);
                         }
                     }
