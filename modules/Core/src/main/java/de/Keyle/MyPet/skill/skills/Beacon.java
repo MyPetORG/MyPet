7b2f0985607dcdd21f1fbfae1d5e64db68e4f9fd
xXKeyleXx <keyle@t-online.de>
2016-03-13 12:42:15 +0100

    fix copyright header

--- modules/Core/src/main/java/de/Keyle/MyPet/skill/skills/Beacon.java
+++ modules/Core/src/main/java/de/Keyle/MyPet/skill/skills/Beacon.java
@@ -1,15 +1,15 @@
 /*
- * This file is part of mypet-core
+ * This file is part of MyPet
  *
- * Copyright (C) 2011-2016 Keyle
- * mypet-core is licensed under the GNU Lesser General Public License.
+ * Copyright © 2011-2016 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
  *
- * mypet-core is free software: you can redistribute it and/or modify
+ * MyPet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * mypet-core is distributed in the hope that it will be useful,
+ * MyPet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
--- modules/v1_7_R4/src/main/java/de/Keyle/MyPet/compat/v1_7_R4/util/iconmenu/IconMenuInventory.java
+++ modules/v1_7_R4/src/main/java/de/Keyle/MyPet/compat/v1_7_R4/util/iconmenu/IconMenuInventory.java
@@ -1,15 +1,15 @@
 /*
- * This file is part of mypet-compat-v1_7_R4
+ * This file is part of MyPet
  *
- * Copyright (C) 2011-2016 Keyle
- * mypet-compat-v1_7_R4 is licensed under the GNU Lesser General Public License.
+ * Copyright © 2011-2016 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
  *
- * mypet-compat-v1_7_R4 is free software: you can redistribute it and/or modify
+ * MyPet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * mypet-compat-v1_7_R4 is distributed in the hope that it will be useful,
+ * MyPet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
