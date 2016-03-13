3efa7133632dfb7b101245f92b055e2d7c8c00b0
xXKeyleXx <keyle@t-online.de>
2016-03-13 18:22:56 +0100

    fix copyright header

--- modules/v1_8_R3/src/main/java/de/Keyle/MyPet/compat/v1_8_R3/entity/types/EntityMyEndermite.java
+++ modules/v1_8_R3/src/main/java/de/Keyle/MyPet/compat/v1_8_R3/entity/types/EntityMyEndermite.java
@@ -1,15 +1,15 @@
 /*
- * This file is part of mypet-v1_8_R3
+ * This file is part of MyPet
  *
- * Copyright (C) 2011-2016 Keyle
- * mypet-v1_8_R3 is licensed under the GNU Lesser General Public License.
+ * Copyright © 2011-2016 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
  *
- * mypet-v1_8_R3 is free software: you can redistribute it and/or modify
+ * MyPet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * mypet-v1_8_R3 is distributed in the hope that it will be useful,
+ * MyPet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
