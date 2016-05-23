afded0f3208305a4c27c01ad6df880e21430a823
xXKeyleXx <keyle@t-online.de>
2016-05-23 18:04:06 +0200

    send nospace message on player join

--- modules/MyPet/src/main/java/de/Keyle/MyPet/listeners/PlayerListener.java
+++ modules/MyPet/src/main/java/de/Keyle/MyPet/listeners/PlayerListener.java
@@ -190,6 +190,9 @@ public class PlayerListener implements Listener {
                                                     case Flying:
                                                         runMyPet.getOwner().sendMessage(Util.formatText(Translation.getString("Message.Spawn.Flying", myPet.getOwner()), myPet.getPetName()));
                                                         break;
+                                                    case NoSpace:
+                                                        runMyPet.getOwner().sendMessage(Util.formatText(Translation.getString("Message.Spawn.NoSpace", myPet.getOwner()), myPet.getPetName()));
+                                                        break;
                                                 }
                                             }
                                         }
