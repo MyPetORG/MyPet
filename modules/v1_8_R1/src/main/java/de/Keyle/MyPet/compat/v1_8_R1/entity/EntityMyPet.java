1f546b785cc8c8d687bc0c5c57a4ff02a8e3ad4a
xXKeyleXx <keyle@t-online.de>
2016-08-09 11:31:21 +0200

    fix riding for compat v1_8_R2 and v1_8_R3

--- modules/v1_8_R1/src/main/java/de/Keyle/MyPet/compat/v1_8_R1/entity/EntityMyPet.java
+++ modules/v1_8_R1/src/main/java/de/Keyle/MyPet/compat/v1_8_R1/entity/EntityMyPet.java
@@ -48,7 +48,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R1.entity.CraftLivingEntity;
-import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_8_R1.inventory.CraftItemStack;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.LivingEntity;
@@ -420,7 +419,6 @@ public abstract class EntityMyPet extends EntityCreature implements IAnimal, MyP
      */
     public boolean handlePlayerInteraction(final EntityHuman entityhuman) {
         final ItemStack itemStack = entityhuman.inventory.getItemInHand();
-        Player owner = this.getOwner().getPlayer();
 
         if (itemStack != null && itemStack.getItem() == Items.LEAD) {
             ((EntityPlayer) entityhuman).playerConnection.sendPacket(new PacketPlayOutAttachEntity(1, this, null));
@@ -434,10 +432,11 @@ public abstract class EntityMyPet extends EntityCreature implements IAnimal, MyP
         }
 
         if (isMyPet() && myPet.getOwner().equals(entityhuman)) {
+            Player owner = this.getOwner().getPlayer();
             if (Configuration.Skilltree.Skill.Ride.RIDE_ITEM.compare(itemStack)) {
                 if (myPet.getSkills().isSkillActive(Ride.class) && canMove()) {
                     if (Permissions.hasExtendedLegacy(owner, "MyPet.extended.ride")) {
-                        ((CraftPlayer) owner).getHandle().mount(this);
+                        entityhuman.mount(this);
                         return true;
                     } else {
                         getOwner().sendMessage(Translation.getString("Message.No.CanUse", myPet.getOwner().getLanguage()));
