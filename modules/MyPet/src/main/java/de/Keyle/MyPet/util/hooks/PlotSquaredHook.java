57bbe6a57a64ac8756f9d445de7966b02476f30d
xXKeyleXx <keyle@t-online.de>
2016-08-09 19:13:45 +0200

    fix PlotSquared spam

--- modules/MyPet/src/main/java/de/Keyle/MyPet/util/hooks/PlotSquaredHook.java
+++ modules/MyPet/src/main/java/de/Keyle/MyPet/util/hooks/PlotSquaredHook.java
@@ -20,14 +20,19 @@
 
 package de.Keyle.MyPet.util.hooks;
 
+import com.intellectualcrafters.plot.flag.Flags;
+import com.intellectualcrafters.plot.object.Plot;
+import com.intellectualcrafters.plot.object.PlotArea;
+import com.intellectualcrafters.plot.object.PlotPlayer;
+import com.intellectualcrafters.plot.util.Permissions;
 import com.plotsquared.bukkit.listeners.PlayerEvents;
+import com.plotsquared.bukkit.util.BukkitUtil;
 import de.Keyle.MyPet.api.Configuration;
 import de.Keyle.MyPet.api.util.hooks.PluginHookName;
 import de.Keyle.MyPet.api.util.hooks.types.PlayerVersusEntityHook;
 import de.Keyle.MyPet.api.util.hooks.types.PlayerVersusPlayerHook;
 import de.Keyle.MyPet.util.PluginHook;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.*;
 
 @PluginHookName("PlotSquared")
 public class PlotSquaredHook extends PluginHook implements PlayerVersusPlayerHook, PlayerVersusEntityHook {
@@ -45,11 +50,123 @@ public class PlotSquaredHook extends PluginHook implements PlayerVersusPlayerHoo
 
     @Override
     public boolean canHurt(Player attacker, Entity defender) {
-        return playerEvents.entityDamage(attacker, defender);
+        com.intellectualcrafters.plot.object.Location dloc = BukkitUtil.getLocation(attacker);
+        com.intellectualcrafters.plot.object.Location vloc = BukkitUtil.getLocation(defender);
+        PlotArea dArea = dloc.getPlotArea();
+        PlotArea vArea = dArea != null && dArea.contains(vloc.getX(), vloc.getZ()) ? dArea : vloc.getPlotArea();
+        if (dArea == null && vArea == null) {
+            return true;
+        } else {
+            Plot dplot = dArea != null ? dArea.getPlot(dloc) : null;
+            Plot vplot = vArea != null ? vArea.getPlot(vloc) : null;
+            Plot plot;
+            String stub;
+            if (dplot == null && vplot == null) {
+                if (dArea == null) {
+                    return true;
+                }
+
+                plot = null;
+                stub = "road";
+            } else {
+                if (defender.getTicksLived() > attacker.getTicksLived()) {
+                    if (dplot != null && defender instanceof Player) {
+                        plot = dplot;
+                    } else if (vplot == null) {
+                        plot = dplot;
+                    } else {
+                        plot = vplot;
+                    }
+                } else if (dplot != null && defender instanceof Player) {
+                    if (vplot == null) {
+                        plot = dplot;
+                    } else {
+                        plot = vplot;
+                    }
+                } else if (vplot == null) {
+                    plot = dplot;
+                } else {
+                    plot = vplot;
+                }
+
+                if (plot.hasOwner()) {
+                    stub = "other";
+                } else {
+                    stub = "unowned";
+                }
+            }
+
+            PlotPlayer plotPlayer1 = BukkitUtil.getPlayer(attacker);
+            if (defender instanceof Hanging) {
+                if (plot != null && (plot.getFlag(Flags.HANGING_BREAK, Boolean.FALSE) || plot.isAdded(plotPlayer1.getUUID()))) {
+                    return true;
+                }
+
+                if (!Permissions.hasPermission(plotPlayer1, "plots.admin.destroy." + stub)) {
+                    return false;
+                }
+            } else if (defender.getEntityId() == 30) {
+                if (plot != null && (plot.getFlag(Flags.MISC_BREAK, Boolean.FALSE) || plot.isAdded(plotPlayer1.getUUID()))) {
+                    return true;
+                }
+
+                if (!Permissions.hasPermission(plotPlayer1, "plots.admin.destroy." + stub)) {
+                    return false;
+                }
+            } else if (!(defender instanceof Monster) && !(defender instanceof EnderDragon)) {
+                if (defender instanceof Tameable) {
+                    if (plot != null && (plot.getFlag(Flags.TAMED_ATTACK, Boolean.FALSE) || plot.getFlag(Flags.PVE, Boolean.FALSE) || plot.isAdded(plotPlayer1.getUUID()))) {
+                        return true;
+                    }
+
+                    if (!Permissions.hasPermission(plotPlayer1, "plots.admin.pve." + stub)) {
+                        return false;
+                    }
+                } else if (defender instanceof Player) {
+                    if (plot != null) {
+                        return !(Flags.PVP.isFalse(plot) && !Permissions.hasPermission(plotPlayer1, "plots.admin.pvp." + stub));
+                    }
+
+                    if (!Permissions.hasPermission(plotPlayer1, "plots.admin.pvp." + stub)) {
+                        return false;
+                    }
+                } else if (!(defender instanceof Creature)) {
+                    if (defender instanceof Vehicle) {
+                        return true;
+                    }
+
+                    if (plot != null && (plot.getFlag(Flags.PVE, Boolean.FALSE) || plot.isAdded(plotPlayer1.getUUID()))) {
+                        return true;
+                    }
+
+                    if (!Permissions.hasPermission(plotPlayer1, "plots.admin.pve." + stub)) {
+                        return false;
+                    }
+                } else {
+                    if (plot != null && (plot.getFlag(Flags.ANIMAL_ATTACK, Boolean.FALSE) || plot.getFlag(Flags.PVE, Boolean.FALSE) || plot.isAdded(plotPlayer1.getUUID()))) {
+                        return true;
+                    }
+
+                    if (!Permissions.hasPermission(plotPlayer1, "plots.admin.pve." + stub)) {
+                        return false;
+                    }
+                }
+            } else {
+                if (plot != null && (plot.getFlag(Flags.HOSTILE_ATTACK, Boolean.FALSE) || plot.getFlag(Flags.PVE, Boolean.FALSE) || plot.isAdded(plotPlayer1.getUUID()))) {
+                    return true;
+                }
+
+                if (!Permissions.hasPermission(plotPlayer1, "plots.admin.pve." + stub)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 
     @Override
     public boolean canHurt(Player attacker, Player defender) {
-        return playerEvents.entityDamage(attacker, defender);
+        return canHurt(attacker, (Entity) defender);
     }
 }
