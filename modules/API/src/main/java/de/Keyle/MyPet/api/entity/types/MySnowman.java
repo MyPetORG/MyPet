4b1f52b66d07ca901f1a2520a6003cd24b992692
xXKeyleXx <keyle@t-online.de>
2016-08-12 16:59:42 +0200

    fix food item for snowmen

--- modules/API/src/main/java/de/Keyle/MyPet/api/entity/types/MySnowman.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/entity/types/MySnowman.java
@@ -23,10 +23,10 @@ package de.Keyle.MyPet.api.entity.types;
 import de.Keyle.MyPet.api.entity.DefaultInfo;
 import de.Keyle.MyPet.api.entity.MyPet;
 
-import static org.bukkit.Material.CARROT;
+import static org.bukkit.Material.CARROT_ITEM;
 import static org.bukkit.Material.SNOW_BALL;
 
-@DefaultInfo(food = {CARROT, SNOW_BALL})
+@DefaultInfo(food = {CARROT_ITEM, SNOW_BALL})
 public interface MySnowman extends MyPet {
     boolean isSheared();
 
