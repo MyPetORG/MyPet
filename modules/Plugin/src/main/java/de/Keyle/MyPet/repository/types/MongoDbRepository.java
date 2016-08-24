373a01ed21c8f2d8fb7aae4e87f6fd373e36afcb
xXKeyleXx <keyle@t-online.de>
2016-08-24 11:45:33 +0200

    fixed/improved/impair maven structure

--- modules/Plugin/src/main/java/de/Keyle/MyPet/repository/types/MongoDbRepository.java
+++ modules/Plugin/src/main/java/de/Keyle/MyPet/repository/types/MongoDbRepository.java
@@ -21,7 +21,6 @@
 package de.Keyle.MyPet.repository.types;
 
 import com.google.common.collect.Lists;
-import com.mongodb.*;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
