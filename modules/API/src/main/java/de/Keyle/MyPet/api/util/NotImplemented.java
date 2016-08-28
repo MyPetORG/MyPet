c9c04064f53fe3e410e74faacde60cc03b975e2b
xXKeyleXx <keyle@t-online.de>
2016-08-28 20:40:30 +0200

    add not implemented annotation

---
+++ modules/API/src/main/java/de/Keyle/MyPet/api/util/NotImplemented.java
@@ -0,0 +1,11 @@
+package de.Keyle.MyPet.api.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NotImplemented {
+}
