190b4eec9b33f8b26ed32047aa896e4bd5321150
xXKeyleXx <keyle@t-online.de>
2016-08-28 20:40:30 +0200

    add not implemented annotation

--- modules/API/src/main/java/de/Keyle/MyPet/api/util/Compat.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/util/Compat.java
@@ -20,6 +20,13 @@
 
 package de.Keyle.MyPet.api.util;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Compat {
     String value();
 }
