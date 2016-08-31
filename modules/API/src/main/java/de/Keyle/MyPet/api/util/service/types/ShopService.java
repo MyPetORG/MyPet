53f19d186f7f981f1772ff859a6b4e1b68d28d16
xXKeyleXx <keyle@t-online.de>
2016-08-31 10:51:04 +0200

    add getShopNames method to shop service

--- modules/API/src/main/java/de/Keyle/MyPet/api/util/service/types/ShopService.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/util/service/types/ShopService.java
@@ -3,8 +3,12 @@ package de.Keyle.MyPet.api.util.service.types;
 import de.Keyle.MyPet.api.util.service.ServiceContainer;
 import org.bukkit.entity.Player;
 
+import java.util.Set;
+
 public interface ShopService extends ServiceContainer {
     void open(String name, Player player);
 
     void open(Player player);
+
+    Set<String> getShopNames();
 }
