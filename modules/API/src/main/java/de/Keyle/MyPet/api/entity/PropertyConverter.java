f130f62907e227af5f755482b1a69f0cbbcdd2c1
xXKeyleXx <keyle@t-online.de>
2017-07-31 22:38:21 +0200

    copyright update

--- modules/API/src/main/java/de/Keyle/MyPet/api/entity/PropertyConverter.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/entity/PropertyConverter.java
@@ -1,20 +1,20 @@
 /*
  * This file is part of MyPet
  *
- * Copyright © 2011-2016 Keyle
- * MyPet is licensed under the GNU Lesser General public static License.
+ * Copyright © 2011-2017 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
  *
  * MyPet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General public static License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * MyPet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General public static License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General public static License
+ * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
--- modules/Plugin/src/main/java/de/Keyle/MyPet/util/ResourcePackManager.java
+++ modules/Plugin/src/main/java/de/Keyle/MyPet/util/ResourcePackManager.java
@@ -1,15 +1,15 @@
 /*
- * This file is part of mypet-plugin_main
+ * This file is part of MyPet
  *
- * Copyright (C) 2011-2016 Keyle
- * mypet-plugin_main is licensed under the GNU Lesser General Public License.
+ * Copyright © 2011-2017 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
  *
- * mypet-plugin_main is free software: you can redistribute it and/or modify
+ * MyPet is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * mypet-plugin_main is distributed in the hope that it will be useful,
+ * MyPet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
