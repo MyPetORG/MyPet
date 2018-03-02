51bdef2b6a9fd3f190ef09a19f6613af3d013b6e
xXKeyleXx <github@keyle.de>
2018-03-02 12:17:41 +0100

    check hashes of the downloaded files from the auto-updater

--- modules/Plugin/src/main/java/de/Keyle/MyPet/util/Updater.java
+++ modules/Plugin/src/main/java/de/Keyle/MyPet/util/Updater.java
@@ -20,6 +20,8 @@
 
 package de.Keyle.MyPet.util;
 
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
 import de.Keyle.MyPet.MyPetApi;
 import de.Keyle.MyPet.api.Configuration;
 import de.Keyle.MyPet.api.MyPetVersion;
@@ -29,10 +31,7 @@ import org.bukkit.ChatColor;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Optional;
@@ -143,6 +142,7 @@ public class Updater {
         } else {
             url += "release";
         }
+        String hashUrl = url + "/hash?api_token=" + Configuration.Update.TOKEN;
         url += "?api_token=" + Configuration.Update.TOKEN;
         File pluginFile;
         if (Configuration.Update.REPLACE_OLD) {
@@ -156,34 +156,33 @@ public class Updater {
 
         String finalUrl = url;
         Runnable downloadRunner = () -> {
+            MyPetApi.getLogger().info(ChatColor.RED + "Start update download: " + ChatColor.RESET + latest);
+            String localHash = "";
+            String remoteHash = "";
+
             try {
-                MyPetApi.getLogger().info(ChatColor.RED + "Start update download: " + ChatColor.RESET + latest);
                 URL website = new URL(finalUrl);
                 HttpURLConnection httpConn = (HttpURLConnection) website.openConnection();
                 int responseCode = httpConn.getResponseCode();
 
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    InputStream inputStream = httpConn.getInputStream();
-                    FileOutputStream outputStream = new FileOutputStream(pluginFile);
-
-                    int bytesRead;
-                    byte[] buffer = new byte[4096];
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        outputStream.write(buffer, 0, bytesRead);
-                    }
-
-                    outputStream.close();
-                    inputStream.close();
-
-                    String message = "Finished update download.";
-                    if (Configuration.Update.REPLACE_OLD || MyPetApi.getPlugin().getFile().getName().equals("MyPet-" + latest.getVersion() + ".jar")) {
-                        message += " The update will be loaded on the next server start.";
-                    } else {
-                        message += " The file was stored in the \"update\" folder.";
-                    }
-                    MyPetApi.getLogger().info(message);
+                if (responseCode != HttpURLConnection.HTTP_OK) {
+                    httpConn.disconnect();
+                    return;
                 }
-                httpConn.disconnect();
+                InputStream inputStream = httpConn.getInputStream();
+                FileOutputStream outputStream = new FileOutputStream(pluginFile);
+                Hasher hasher = Hashing.sha256().newHasher();
+
+                int bytesRead;
+                byte[] buffer = new byte[4096];
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                    hasher.putBytes(buffer, 0, bytesRead);
+                }
+                localHash = hasher.hash().toString();
+
+                outputStream.close();
+                inputStream.close();
             } catch (IOException e) {
                 if (e.getMessage().contains("403")) {
                     MyPetApi.getLogger().warning(ChatColor.RED + "You are not allowed to download MyPet-Premium. Please check/set your download token in the config.");
@@ -193,8 +192,42 @@ public class Updater {
                     e.printStackTrace();
                 }
             }
+
+            // Check hash now to be sure we downloaded the correct file
+            try {
+                URL website = new URL(hashUrl);
+                HttpURLConnection httpConn = (HttpURLConnection) website.openConnection();
+                int responseCode = httpConn.getResponseCode();
+
+                boolean skipHash = responseCode != HttpURLConnection.HTTP_OK;
+                if (skipHash) {
+                    httpConn.disconnect();
+                } else {
+                    InputStream inputStream = new BufferedInputStream(httpConn.getInputStream());
+                    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                    String inputLine;
+                    while ((inputLine = br.readLine()) != null) {
+                        remoteHash += inputLine;
+                    }
+                    inputStream.close();
+                }
+            } catch (IOException ignored) {
+                ignored.printStackTrace();
+            }
+
+            if (remoteHash.isEmpty() || (!localHash.isEmpty() && localHash.equalsIgnoreCase(remoteHash))) {
+                String message = "Finished update download.";
+                if (Configuration.Update.REPLACE_OLD || MyPetApi.getPlugin().getFile().getName().equals("MyPet-" + latest.getVersion() + ".jar")) {
+                    message += " The update will be loaded on the next server start.";
+                } else {
+                    message += " The file was stored in the \"update\" folder.";
+                }
+                MyPetApi.getLogger().info(message);
+            } else {
+                MyPetApi.getLogger().warning(ChatColor.RED + "Update failed! Try again or download it manually.");
+            }
         };
-        if (Configuration.Update.ASYNC) {
+        if (!Configuration.Update.ASYNC) {
             downloadRunner.run();
         } else {
             thread = new Thread(downloadRunner);
@@ -203,7 +236,7 @@ public class Updater {
     }
 
     public void waitForDownload() {
-        if (!Configuration.Update.ASYNC && thread != null && thread.isAlive()) {
+        if (Configuration.Update.ASYNC && thread != null && thread.isAlive()) {
             MyPetApi.getLogger().info("Wait for the update download to finish...");
             try {
                 thread.join();
