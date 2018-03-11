fbeab46867a2ab02e91a65c1b166d758ab6232fe
xXKeyleXx <github@keyle.de>
2018-03-11 18:07:30 +0100

    move ranged skill entities to better package

--- modules/API/src/main/java/de/Keyle/MyPet/api/entity/skill/ranged/CraftMyPetProjectile.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/entity/skill/ranged/CraftMyPetProjectile.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of MyPet
  *
- * Copyright © 2011-2017 Keyle
+ * Copyright © 2011-2018 Keyle
  * MyPet is licensed under the GNU Lesser General Public License.
  *
  * MyPet is free software: you can redistribute it and/or modify
@@ -18,7 +18,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package de.Keyle.MyPet.api.skill.skills.ranged;
+package de.Keyle.MyPet.api.entity.skill.ranged;
 
 import de.Keyle.MyPet.api.entity.MyPetBukkitEntity;
 import org.bukkit.entity.Projectile;
--- modules/v1_8_R1/src/main/java/de/Keyle/MyPet/compat/v1_8_R1/skill/skills/ranged/nms/MyPetSmallFireball.java
+++ modules/v1_8_R1/src/main/java/de/Keyle/MyPet/compat/v1_8_R1/skill/skills/ranged/nms/MyPetSmallFireball.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of MyPet
  *
- * Copyright © 2011-2017 Keyle
+ * Copyright © 2011-2018 Keyle
  * MyPet is licensed under the GNU Lesser General Public License.
  *
  * MyPet is free software: you can redistribute it and/or modify
@@ -20,7 +20,7 @@
 
 package de.Keyle.MyPet.compat.v1_8_R1.skill.skills.ranged.nms;
 
-import de.Keyle.MyPet.api.skill.skills.ranged.EntityMyPetProjectile;
+import de.Keyle.MyPet.api.entity.skill.ranged.EntityMyPetProjectile;
 import de.Keyle.MyPet.api.util.Compat;
 import de.Keyle.MyPet.compat.v1_8_R1.entity.EntityMyPet;
 import de.Keyle.MyPet.compat.v1_8_R1.skill.skills.ranged.bukkit.CraftMyPetSmallFireball;
