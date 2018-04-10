6932deebce80e23bff772b85c119425a5702bf2a
xXKeyleXx <github@keyle.de>
2018-04-10 10:11:32 +0200

    load the correct EntityConverterService in 1.8-R1 fixes #1111

--- modules/v1_8_R1/src/main/java/de/Keyle/MyPet/compat/v1_8_R1/CompatManager.java
+++ modules/v1_8_R1/src/main/java/de/Keyle/MyPet/compat/v1_8_R1/CompatManager.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of MyPet
  *
- * Copyright © 2011-2017 Keyle
+ * Copyright © 2011-2018 Keyle
  * MyPet is licensed under the GNU Lesser General Public License.
  *
  * MyPet is free software: you can redistribute it and/or modify
@@ -22,8 +22,8 @@ package de.Keyle.MyPet.compat.v1_8_R1;
 
 import de.Keyle.MyPet.MyPetApi;
 import de.Keyle.MyPet.api.util.Compat;
-import de.Keyle.MyPet.api.util.service.types.EntityConverterService;
 import de.Keyle.MyPet.compat.v1_8_R1.services.EggIconService;
+import de.Keyle.MyPet.compat.v1_8_R1.services.EntityConverterService;
 
 @Compat("v1_8_R1")
 public class CompatManager extends de.Keyle.MyPet.api.util.CompatManager {
