c9ca46282db5f65c765b983d27c9d03b9d00edec
xXKeyleXx <github@keyle.de>
2018-04-22 17:52:08 +0200

    disable services correctly

--- modules/API/src/main/java/de/Keyle/MyPet/api/util/service/ServiceManager.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/util/service/ServiceManager.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of MyPet
  *
- * Copyright © 2011-2017 Keyle
+ * Copyright © 2011-2018 Keyle
  * MyPet is licensed under the GNU Lesser General Public License.
  *
  * MyPet is free software: you can redistribute it and/or modify
@@ -100,7 +100,7 @@ public class ServiceManager {
 
     public void disableServices() {
         for (ServiceContainer service : serviceByName.values()) {
-            service.onEnable();
+            service.onDisable();
         }
     }
 
