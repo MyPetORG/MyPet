63f3c92758535e1c5f82deb7fe6f2aaa4c859397
xXKeyleXx <github@keyle.de>
2018-07-15 20:17:52 +0200

    add Gangs+ hook

--- modules/Plugin/src/main/java/de/Keyle/MyPet/MyPetPlugin.java
+++ modules/Plugin/src/main/java/de/Keyle/MyPet/MyPetPlugin.java
@@ -493,6 +493,7 @@ public class MyPetPlugin extends JavaPlugin implements de.Keyle.MyPet.api.plugin
         pluginHookManager.registerHook(BossShopProHook.class);
         pluginHookManager.registerHook(CitizensHook.class);
         pluginHookManager.registerHook(FactionsHook.class);
+        pluginHookManager.registerHook(GangsPlusHook.class);
         pluginHookManager.registerHook(GriefPreventionHook.class);
         pluginHookManager.registerHook(GriefPreventionPlusHook.class);
         pluginHookManager.registerHook(HeroesHook.class);
