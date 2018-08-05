ed841cc3443bfa41200bb99ce3af6d4a46dbd0c1
xXKeyleXx <github@keyle.de>
2018-08-05 16:54:59 +0200

    add more projectiles to the Ranged skill

---
+++ modules/v1_9_R2/src/main/java/de/Keyle/MyPet/compat/v1_9_R2/skill/skills/ranged/nms/MyPetEnderPearl.java
@@ -0,0 +1,80 @@
+/*
+ * This file is part of MyPet
+ *
+ * Copyright © 2011-2018 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
+ *
+ * MyPet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyPet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.Keyle.MyPet.compat.v1_9_R2.skill.skills.ranged.nms;
+
+import de.Keyle.MyPet.api.entity.skill.ranged.EntityMyPetProjectile;
+import de.Keyle.MyPet.api.util.Compat;
+import de.Keyle.MyPet.compat.v1_9_R2.entity.EntityMyPet;
+import de.Keyle.MyPet.compat.v1_9_R2.skill.skills.ranged.bukkit.CraftMyPetEnderPearl;
+import net.minecraft.server.v1_9_R2.*;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftEntity;
+
+@Compat("v1_9_R2")
+public class MyPetEnderPearl extends EntityEnderPearl implements EntityMyPetProjectile {
+
+    protected float damage = 0;
+
+    public MyPetEnderPearl(World world, EntityMyPet entityLiving) {
+        super(world, entityLiving);
+    }
+
+    @Override
+    public EntityMyPet getShooter() {
+        return (EntityMyPet) this.shooter;
+    }
+
+    public void setDamage(float damage) {
+        this.damage = damage;
+    }
+
+    @Override
+    public CraftEntity getBukkitEntity() {
+        if (this.bukkitEntity == null) {
+            this.bukkitEntity = new CraftMyPetEnderPearl(this.world.getServer(), this);
+        }
+        return this.bukkitEntity;
+    }
+
+    @Override
+    public void a(NBTTagCompound nbtTagCompound) {
+    }
+
+    @Override
+    public void b(NBTTagCompound nbtTagCompound) {
+    }
+
+    @Override
+    protected void a(MovingObjectPosition movingObjectPosition) {
+        if (movingObjectPosition.entity != null) {
+            if (movingObjectPosition.entity instanceof EntityLiving) {
+                movingObjectPosition.entity.damageEntity(DamageSource.projectile(this, getShooter()), damage);
+            }
+        }
+        for (int i = 0; i < 32; ++i) {
+            this.world.addParticle(EnumParticle.PORTAL, this.locX, this.locY + this.random.nextDouble() * 2.0D, this.locZ, this.random.nextGaussian(), 0.0D, this.random.nextGaussian(), new int[0]);
+        }
+        die();
+    }
+
+    public boolean damageEntity(DamageSource damagesource, float f) {
+        return false;
+    }
+}
