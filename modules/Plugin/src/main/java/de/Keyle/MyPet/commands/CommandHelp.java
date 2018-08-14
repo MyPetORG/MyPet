e503aa3f8ec82b40f6485325e0147d5c3380d490
xXKeyleXx <github@keyle.de>
2018-08-14 11:57:36 +0200

    remove /pettype command

--- modules/Plugin/src/main/java/de/Keyle/MyPet/commands/CommandHelp.java
+++ modules/Plugin/src/main/java/de/Keyle/MyPet/commands/CommandHelp.java
@@ -47,7 +47,6 @@ public class CommandHelp implements CommandExecutor, TabCompleter {
                 player.sendMessage(Util.formatText(Translation.getString("Message.Command.Help.Admin", player), "/petadmin"));
             }
             player.sendMessage(Util.formatText(Translation.getString("Message.Command.Help.Info", player), "/petinfo"));
-            player.sendMessage(Util.formatText(Translation.getString("Message.Command.Help.Type", player), "/pettype"));
             player.sendMessage(Util.formatText(Translation.getString("Message.Command.Help.Options", player), "/petoptions"));
             if (MyPetApi.getMyPetManager().hasActiveMyPet(player)) {
                 player.sendMessage(Util.formatText(Translation.getString("Message.Command.Help.Call", player), "/petcall"));
