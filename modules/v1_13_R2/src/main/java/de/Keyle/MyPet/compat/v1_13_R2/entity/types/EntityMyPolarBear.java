177471900be5d0d9bf7d99c6572e2142290ecb80
xXKeyleXx <github@keyle.de>
2018-08-28 17:15:51 +0200

    add 1.13.1 support

---
+++ modules/v1_13_R2/src/main/java/de/Keyle/MyPet/compat/v1_13_R2/entity/types/EntityMyPolarBear.java
@@ -0,0 +1,120 @@
+/*
+ * This file is part of MyPet
+ *
+ * Copyright © 2011-2018 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
+ *
+ * MyPet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyPet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.Keyle.MyPet.compat.v1_13_R2.entity.types;
+
+import de.Keyle.MyPet.api.Configuration;
+import de.Keyle.MyPet.api.entity.EntitySize;
+import de.Keyle.MyPet.api.entity.MyPet;
+import de.Keyle.MyPet.api.entity.types.MyPolarBear;
+import de.Keyle.MyPet.compat.v1_13_R2.entity.EntityMyPet;
+import net.minecraft.server.v1_13_R2.*;
+
+@EntitySize(width = 1.3F, height = 1.4F)
+public class EntityMyPolarBear extends EntityMyPet {
+
+    private static final DataWatcherObject<Boolean> ageWatcher = DataWatcher.a(EntityMyPolarBear.class, DataWatcherRegistry.i);
+    private static final DataWatcherObject<Boolean> rearWatcher = DataWatcher.a(EntityMyPolarBear.class, DataWatcherRegistry.i);
+
+    int rearCounter = -1;
+
+    public EntityMyPolarBear(World world, MyPet myPet) {
+        super(EntityTypes.POLAR_BEAR, world, myPet);
+    }
+
+    @Override
+    protected String getDeathSound() {
+        return "entity.polar_bear.death";
+    }
+
+    @Override
+    protected String getHurtSound() {
+        return "entity.polar_bear.hurt";
+    }
+
+    protected String getLivingSound() {
+        if (getMyPet().isBaby()) {
+            return "entity.polar_bear.baby_ambient";
+        }
+        return "entity.polar_bear.ambient";
+    }
+
+    public boolean handlePlayerInteraction(EntityHuman entityhuman, EnumHand enumhand, ItemStack itemStack) {
+        if (super.handlePlayerInteraction(entityhuman, enumhand, itemStack)) {
+            return true;
+        }
+
+        if (getOwner().equals(entityhuman) && itemStack != null && canUseItem()) {
+            if (Configuration.MyPet.Cow.GROW_UP_ITEM.compare(itemStack) && getMyPet().isBaby() && getOwner().getPlayer().isSneaking()) {
+                if (!entityhuman.abilities.canInstantlyBuild) {
+                    itemStack.subtract(1);
+                    if (itemStack.getCount() <= 0) {
+                        entityhuman.inventory.setItem(entityhuman.inventory.itemInHandIndex, ItemStack.a);
+                    }
+                }
+                getMyPet().setBaby(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected void initDatawatcher() {
+        super.initDatawatcher();
+        this.datawatcher.register(ageWatcher, false);
+        this.datawatcher.register(rearWatcher, false);
+
+    }
+
+    public boolean attack(Entity entity) {
+        boolean flag = false;
+        try {
+            flag = super.attack(entity);
+            if (flag) {
+                this.datawatcher.set(rearWatcher, true);
+                rearCounter = 10;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
+
+    public void onLivingUpdate() {
+        super.onLivingUpdate();
+        if (rearCounter > -1 && rearCounter-- == 0) {
+            this.datawatcher.set(rearWatcher, false);
+            rearCounter = -1;
+        }
+    }
+
+    @Override
+    public void updateVisuals() {
+        this.datawatcher.set(ageWatcher, getMyPet().isBaby());
+    }
+
+    public void playPetStepSound() {
+        makeSound("entity.polar_bear.step", 0.15F, 1.0F);
+    }
+
+    public MyPolarBear getMyPet() {
+        return (MyPolarBear) myPet;
+    }
+}
---
+++ modules/v1_13_R2/src/main/java/de/Keyle/MyPet/compat/v1_13_R2/util/ConfigItem.java
@@ -0,0 +1,128 @@
+/*
+ * This file is part of MyPet
+ *
+ * Copyright © 2011-2018 Keyle
+ * MyPet is licensed under the GNU Lesser General Public License.
+ *
+ * MyPet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyPet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.Keyle.MyPet.compat.v1_13_R2.util;
+
+import de.Keyle.MyPet.MyPetApi;
+import de.Keyle.MyPet.api.Util;
+import de.Keyle.MyPet.api.util.Compat;
+import de.Keyle.MyPet.compat.v1_13_R2.util.inventory.ItemStackComparator;
+import net.minecraft.server.v1_13_R2.*;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_13_R2.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+
+@Compat("v1_13_R2")
+public class ConfigItem extends de.Keyle.MyPet.api.util.ConfigItem {
+
+    public ConfigItem(ItemStack item, DurabilityMode durabilityMode) {
+        super(item, durabilityMode);
+    }
+
+    public ConfigItem(String data) {
+        super(data);
+    }
+
+    @Override
+    public boolean compare(ItemStack compareItem) {
+        boolean result = super.compare(compareItem);
+        if (result && item != null && item.hasItemMeta()) {
+            if (!ItemStackComparator.compareTagData(item, compareItem)) {
+                return false;
+            }
+        }
+        return result;
+    }
+
+    public boolean compare(Object o) {
+        net.minecraft.server.v1_13_R2.ItemStack compareItem = (net.minecraft.server.v1_13_R2.ItemStack) o;
+        if (item == null || item.getType() == Material.AIR) {
+            return compareItem == null || Item.getId(compareItem.getItem()) == 0;
+        }
+        if (compareItem == null) {
+            return false;
+        }
+        return item.isSimilar(CraftItemStack.asCraftMirror(compareItem));
+    }
+
+    public void load(String data) {
+
+        String[] splitData = data.split("\\s+", 3);
+
+        if (splitData.length == 0) {
+            return;
+        }
+
+        net.minecraft.server.v1_13_R2.ItemStack is;
+
+        Item item;
+        if (Util.isInt(splitData[0])) {
+            int itemId = Integer.parseInt(splitData[0]);
+            item = Item.getById(itemId);
+        } else {
+            MinecraftKey key = new MinecraftKey(splitData[0]);
+            item = IRegistry.ITEM.get(key);
+            if (item == null) {
+                Block block = IRegistry.BLOCK.get(key);
+                item = block.getItem();
+            }
+        }
+        if (item == null) {
+            return;
+        }
+
+        is = new net.minecraft.server.v1_13_R2.ItemStack(item, 1);
+
+        if (splitData.length >= 2) {
+            String dataString = splitData[1];
+            if (dataString.startsWith("<")) {
+                this.durabilityMode = DurabilityMode.Smaller;
+                dataString = dataString.substring(1);
+            } else if (dataString.startsWith(">")) {
+                this.durabilityMode = DurabilityMode.Bigger;
+                dataString = dataString.substring(1);
+            } else {
+                this.durabilityMode = DurabilityMode.Equal;
+            }
+            if (Util.isInt(dataString)) {
+                is.setDamage(Integer.parseInt(dataString));
+            }
+        }
+
+        if (splitData.length == 3) {
+            NBTBase nbtBase = null;
+            String nbtString = splitData[2];
+            if (nbtString.startsWith("{")) {
+                try {
+                    nbtBase = MojangsonParser.parse(nbtString.trim());
+                } catch (Exception e) {
+                    MyPetApi.getLogger().warning("Error" + ChatColor.RESET + " in config: " + ChatColor.UNDERLINE + e.getLocalizedMessage() + ChatColor.RESET + " caused by:");
+                    MyPetApi.getLogger().warning(item.getName() + " " + is.getDamage() + nbtString);
+                }
+                if (nbtBase != null) {
+                    is.setTag((NBTTagCompound) nbtBase);
+                }
+            }
+        }
+
+        this.item = CraftItemStack.asCraftMirror(is);
+    }
+}
