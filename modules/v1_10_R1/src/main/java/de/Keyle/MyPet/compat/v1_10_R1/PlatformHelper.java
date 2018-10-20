05f765adb9dd70f4d1a9fe0aa8d5c94d126ff98e
xXKeyleXx <github@keyle.de>
2018-10-20 12:43:23 +0200

    check legacy IDs before the legacy names

--- modules/v1_10_R1/src/main/java/de/Keyle/MyPet/compat/v1_10_R1/PlatformHelper.java
+++ modules/v1_10_R1/src/main/java/de/Keyle/MyPet/compat/v1_10_R1/PlatformHelper.java
@@ -309,7 +309,11 @@ public class PlatformHelper extends de.Keyle.MyPet.api.PlatformHelper {
     }
 
     public Material getMaterial(MaterialHolder materialHolder) {
-        return Material.matchMaterial(materialHolder.getLegacyName().getName());
+        Material mat = Material.getMaterial(materialHolder.getLegacyId().getId());
+        if (mat == null) {
+            mat = Material.matchMaterial(materialHolder.getLegacyName().getName());
+        }
+        return mat;
     }
 
     @Override
