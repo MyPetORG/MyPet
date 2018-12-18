f3a969923bb89c7e005bebf844dca183fc8c349e
xXKeyleXx <github@keyle.de>
2018-12-18 21:30:44 +0100

    allow underline in color names too

--- modules/API/src/main/java/de/Keyle/MyPet/api/util/Colorizer.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/util/Colorizer.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of MyPet
  *
- * Copyright © 2011-2017 Keyle
+ * Copyright © 2011-2018 Keyle
  * MyPet is licensed under the GNU Lesser General Public License.
  *
  * MyPet is free software: you can redistribute it and/or modify
@@ -49,6 +49,7 @@ public class Colorizer {
     static {
         for (ChatColor color : ChatColor.values()) {
             colorCodes.put(color.name().replace("_", ""), String.valueOf(color.getChar()));
+            colorCodes.put(color.name(), String.valueOf(color.getChar()));
         }
     }
 }
