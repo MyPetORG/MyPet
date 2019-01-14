82de7a3b71b1c88d2a1a084e3365a3aacb68d35d
xXKeyleXx <github@keyle.de>
2019-01-14 12:13:52 +0100

    prevent level rules with modulo smaller than 1

--- modules/API/src/main/java/de/Keyle/MyPet/api/skill/skilltree/SkillTreeLoaderJSON.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/skill/skilltree/SkillTreeLoaderJSON.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of MyPet
  *
- * Copyright © 2011-2018 Keyle
+ * Copyright © 2011-2019 Keyle
  * MyPet is licensed under the GNU Lesser General Public License.
  *
  * MyPet is free software: you can redistribute it and/or modify
@@ -209,7 +209,7 @@ public class SkillTreeLoaderJSON {
             Matcher matcher = LEVEL_RULE_REGEX.matcher(levelRuleString);
             while (matcher.find()) {
                 if (matcher.group(0).startsWith("%")) {
-                    modulo = Integer.parseInt(matcher.group(1));
+                    modulo = Math.max(1, Integer.parseInt(matcher.group(1)));
                 } else if (matcher.group(0).startsWith(">")) {
                     min = Integer.parseInt(matcher.group(3));
                 } else if (matcher.group(0).startsWith("<")) {
