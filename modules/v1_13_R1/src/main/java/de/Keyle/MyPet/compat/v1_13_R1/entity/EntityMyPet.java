da4b7ed541f733c7f858526482461e8c83a7f9af
xXKeyleXx <github@keyle.de>
2019-01-16 17:48:46 +0100

    don't allow pets to dive when they are ridden

--- modules/v1_13_R1/src/main/java/de/Keyle/MyPet/compat/v1_13_R1/entity/EntityMyPet.java
+++ modules/v1_13_R1/src/main/java/de/Keyle/MyPet/compat/v1_13_R1/entity/EntityMyPet.java
@@ -1061,6 +1061,10 @@ public abstract class EntityMyPet extends EntityCreature implements IAnimal, MyP
 
         EntityLiving passenger = (EntityLiving) this.getFirstPassenger();
 
+        if (passenger.a(TagsFluid.a)) {
+            this.motY += 0.4;
+        }
+
         Ride rideSkill = myPet.getSkills().get(RideImpl.class);
         if (rideSkill == null || !rideSkill.getActive().getValue()) {
             passenger.stopRiding();
