da2a3a349770e62e21d8621746a6d2112030f5cf
xXKeyleXx <github@keyle.de>
2019-07-11 21:49:18 +0200

    update copyright

--- modules/v1_13_R2/src/main/java/de/Keyle/MyPet/compat/v1_13_R2/entity/types/EntityMyWitch.java
+++ modules/v1_13_R2/src/main/java/de/Keyle/MyPet/compat/v1_13_R2/entity/types/EntityMyWitch.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of MyPet
  *
- * Copyright © 2011-2018 Keyle
+ * Copyright © 2011-2019 Keyle
  * MyPet is licensed under the GNU Lesser General Public License.
  *
  * MyPet is free software: you can redistribute it and/or modify
