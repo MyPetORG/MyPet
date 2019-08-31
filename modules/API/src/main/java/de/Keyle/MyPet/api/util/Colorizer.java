61e3b3fbb99549fedf543fd24f68295e3f688410
xXKeyleXx <github@keyle.de>
2019-08-31 11:39:54 +0200

    use ChatColor method to translate alternate (&) chat colors

--- modules/API/src/main/java/de/Keyle/MyPet/api/util/Colorizer.java
+++ modules/API/src/main/java/de/Keyle/MyPet/api/util/Colorizer.java
@@ -26,6 +26,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Colorizer {
+
     private static Map<String, String> colorCodes = new HashMap<>();
 
     public static String setColors(String text) {
@@ -33,7 +34,7 @@ public class Colorizer {
             text = text.replaceAll("(?i)<" + color + ">", ChatColor.COLOR_CHAR + colorCodes.get(color));
         }
         text = text.replaceAll("(?i)<([0-9a-fk-or])>", ChatColor.COLOR_CHAR + "$1");
-        text = text.replaceAll("(?i)&([0-9a-fk-or])", ChatColor.COLOR_CHAR + "$1");
+        text = ChatColor.translateAlternateColorCodes('&', text);
         return text;
     }
 
