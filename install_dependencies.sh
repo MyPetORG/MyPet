c8984a86fbda0e76180f6213532a8e4e6a8e9019
nafu-at <koucha@nafuchoco.one>
2021-08-05 18:06:56 +0900

    Add the dependencies needed for the build. :1.17

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,6 +7,7 @@ V1_9_R2_S="https://cdn.getbukkit.org/spigot/spigot-1.9.4-R0.1-SNAPSHOT-latest.ja
 V1_12_R1_S="https://cdn.getbukkit.org/spigot/spigot-1.12.jar"
 V1_16_R1_S="https://cdn.getbukkit.org/spigot/spigot-1.16.1.jar"
 V1_16_R2_S="https://cdn.getbukkit.org/spigot/spigot-1.16.5.jar"
+V1_17_R1_S = "https://download.getbukkit.org/spigot/spigot-1.17.jar"
 
 mkdir -p ./dl
 
@@ -15,6 +16,7 @@ wget $V1_9_R2_S -O "./dl/1_9_R2_S.jar" --no-verbose
 wget $V1_12_R1_S -O "./dl/1_12_R1_S.jar" --no-verbose
 wget $V1_16_R1_S -O "./dl/1_16_R1_S.jar" --no-verbose
 wget $V1_16_R2_S -O "./dl/1_16_R2_S.jar" --no-verbose
+wget $V1_17_R1_S -O "./dl/1_17_R1_S.jar" --no-verbose
 
 
 mvn install:install-file -D"file=./dl/1_8_R3_S.jar" -D"version=1.8.8-R0.1-SNAPSHOT" -D"groupId=org.spigotmc" -D"artifactId=spigot" -D"packaging=jar"
@@ -22,5 +24,6 @@ mvn install:install-file -D"file=./dl/1_9_R2_S.jar" -D"version=1.9.4-R0.1-SNAPSH
 mvn install:install-file -D"file=./dl/1_12_R1_S.jar" -D"version=1.12-R0.1-SNAPSHOT" -D"groupId=org.spigotmc" -D"artifactId=spigot" -D"packaging=jar"
 mvn install:install-file -D"file=./dl/1_16_R1_S.jar" -D"version=1.16.1-R0.1-SNAPSHOT" -D"groupId=org.spigotmc" -D"artifactId=spigot" -D"packaging=jar"
 mvn install:install-file -D"file=./dl/1_16_R2_S.jar" -D"version=1.16.5-R0.1-SNAPSHOT" -D"groupId=org.spigotmc" -D"artifactId=spigot" -D"packaging=jar"
+mvn install:install-file -D"file=.\dl\1_17_R1_S.jar" -D"version=1.17-R0.1-SNAPSHOT" -D"groupId=org.spigotmc" -D"artifactId=spigot" -D"packaging=jar"
 
 #rm -r './dl'
