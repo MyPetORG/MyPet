576c040a60d94451bc700c8c6b3db636f8db2ebd
Jakl <57708725+Jakllp@users.noreply.github.com>
2024-02-04 13:39:38 +0100

    Change call PlayerPickupEvent to EntityPickupEvent
    Deprecation.

--- modules/Skills/src/main/java/de/Keyle/MyPet/skill/skills/PickupImpl.java
+++ modules/Skills/src/main/java/de/Keyle/MyPet/skill/skills/PickupImpl.java
@@ -41,7 +41,7 @@ import org.bukkit.GameMode;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.entity.Item;
-import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class PickupImpl implements Pickup {
@@ -136,10 +136,10 @@ public class PickupImpl implements Pickup {
                                     continue;
                                 }
 
-                                PlayerPickupItemEvent playerPickupEvent = new PlayerPickupItemEvent(myPet.getOwner().getPlayer(), itemEntity, 0);
-                                Bukkit.getServer().getPluginManager().callEvent(playerPickupEvent);
+                                EntityPickupItemEvent entityPickupEvent = new EntityPickupItemEvent(myPet.getOwner().getPlayer(), itemEntity, 0);
+                                Bukkit.getServer().getPluginManager().callEvent(entityPickupEvent);
 
-                                if (playerPickupEvent.isCancelled()) {
+                                if (entityPickupEvent.isCancelled()) {
                                     continue;
                                 }
 
