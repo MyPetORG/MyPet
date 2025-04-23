915aa3ebf839c5b986cac89063e79a990af21a5b
Philip Hell <blvckbytes@gmail.com>
2025-04-23 17:38:54 +0200

    modules/v1_21_R4: migrated VariantConverter to new registry-access

--- modules/v1_21_R4/src/main/java/de/Keyle/MyPet/compat/v1_21_R4/util/VariantConverter.java
+++ modules/v1_21_R4/src/main/java/de/Keyle/MyPet/compat/v1_21_R4/util/VariantConverter.java
@@ -1,54 +1,62 @@
 package de.Keyle.MyPet.compat.v1_21_R4.util;
 
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.animal.CatVariant;
-import net.minecraft.world.entity.animal.FrogVariant;
+import net.minecraft.world.entity.animal.CatVariants;
+import net.minecraft.world.entity.animal.frog.FrogVariant;
+import net.minecraft.world.entity.animal.frog.FrogVariants;
+import org.bukkit.craftbukkit.v1_21_R4.CraftRegistry;
 import org.bukkit.entity.Frog;
 
 /**
  * Converts numerical variants into fancy new variants
  */
 public class VariantConverter {
-    private static enum CatVariants {
-        TABBY((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.TABBY).value()),
-        BLACK((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.BLACK).value()),
-        RED((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.RED).value()),
-        SIAMESE((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.SIAMESE).value()),
-        BRITISH_SHORTHAIR((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.BRITISH_SHORTHAIR).value()),
-        CALICO((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.CALICO).value()),
-        PERSIAN((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.PERSIAN).value()),
-        RAGDOLL((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.RAGDOLL).value()),
-        WHITE((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.WHITE).value()),
-        JELLIE((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.JELLIE).value()),
-        ALL_BLACK((CatVariant) BuiltInRegistries.CAT_VARIANT.getOrThrow(CatVariant.ALL_BLACK).value());
+
+    public static final Registry<FrogVariant> FROG_REGISTRY = CraftRegistry.getMinecraftRegistry(Registries.FROG_VARIANT);
+    public static final Registry<CatVariant> CAT_REGISTRY = CraftRegistry.getMinecraftRegistry(Registries.CAT_VARIANT);
+
+    private static enum ConverterCatVariants {
+        TABBY(CAT_REGISTRY.getValue(CatVariants.TABBY)),
+        BLACK(CAT_REGISTRY.getValue(CatVariants.BLACK)),
+        RED(CAT_REGISTRY.getValue(CatVariants.RED)),
+        SIAMESE(CAT_REGISTRY.getValue(CatVariants.SIAMESE)),
+        BRITISH_SHORTHAIR(CAT_REGISTRY.getValue(CatVariants.BRITISH_SHORTHAIR)),
+        CALICO(CAT_REGISTRY.getValue(CatVariants.CALICO)),
+        PERSIAN(CAT_REGISTRY.getValue(CatVariants.PERSIAN)),
+        RAGDOLL(CAT_REGISTRY.getValue(CatVariants.RAGDOLL)),
+        WHITE(CAT_REGISTRY.getValue(CatVariants.WHITE)),
+        JELLIE(CAT_REGISTRY.getValue(CatVariants.JELLIE)),
+        ALL_BLACK(CAT_REGISTRY.getValue(CatVariants.ALL_BLACK));
 
         CatVariant variant;
-        CatVariants(CatVariant cV) {
+        ConverterCatVariants(CatVariant cV) {
             this.variant = cV;
         }
     }
 
-    private static enum FrogVariants {
-        TEMPERATE(BuiltInRegistries.FROG_VARIANT.getOrThrow(FrogVariant.TEMPERATE).value(), Frog.Variant.TEMPERATE),
-        WARM(BuiltInRegistries.FROG_VARIANT.getOrThrow(FrogVariant.WARM).value(), Frog.Variant.WARM),
-        COLD(BuiltInRegistries.FROG_VARIANT.getOrThrow(FrogVariant.COLD).value(), Frog.Variant.COLD);
+    private static enum ConverterFrogVariants {
+        TEMPERATE(FROG_REGISTRY.getValue(FrogVariants.TEMPERATE), Frog.Variant.TEMPERATE),
+        WARM(FROG_REGISTRY.getValue(FrogVariants.WARM), Frog.Variant.WARM),
+        COLD(FROG_REGISTRY.getValue(FrogVariants.COLD), Frog.Variant.COLD);
 
         FrogVariant variant;
         Frog.Variant bukkitVariant;
-        FrogVariants(FrogVariant fV, Frog.Variant fV2) {
+        ConverterFrogVariants(FrogVariant fV, Frog.Variant fV2) {
             this.variant = fV;
             this.bukkitVariant = fV2;
         }
     }
 
     public static CatVariant convertCatVariant(int varId) {
-        return CatVariants.values()[varId].variant;
+        return ConverterCatVariants.values()[varId].variant;
     }
 
     public static FrogVariant convertFrogVariant(int varId) {
-        return FrogVariants.values()[varId].variant;
+        return ConverterFrogVariants.values()[varId].variant;
     }
     public static Frog.Variant getBukkitFrogVariant(int varId) {
-        return FrogVariants.values()[varId].bukkitVariant;
+        return ConverterFrogVariants.values()[varId].bukkitVariant;
     }
 }
