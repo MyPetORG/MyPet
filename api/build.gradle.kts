523a2cebd94c50e02b1770f08e90f819a9f8a842
UserDerezzed <me@userderezzed.dev>
2025-11-04 15:09:25 -0800

    Attempted GH Actions fix

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
 
     compileOnly("org.fusesource.jansi:jansi:1.18")
-    compileOnly("com.google.code.gson:gson:2.8.0")
+    compileOnly("com.google.code.gson:gson:2.8.9")
     compileOnly("de.keyle:knbt:0.0.5")
     compileOnly("at.blvckbytes:RawMessage:0.2")
     compileOnly("org.jetbrains:annotations:16.0.2")
