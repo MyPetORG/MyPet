79d2adb40d1d4a97f35bb3d49bb7d665972d54cc
UserDerezzed <me@userderezzed.dev>
2025-12-04 00:42:37 -0800

    Update exp.js

--- src/main/resources/exp.js
+++ src/main/resources/exp.js
@@ -1,63 +1,26 @@
-/*
- * This file is part of MyPet
- *
- * Copyright © 2011-2019 Keyle
- * MyPet is licensed under the GNU Lesser General Public License.
- *
- * MyPet is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MyPet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+// MyPet XP Level costs:
+//   Levels 2-17:  17 XP each (flat)
+//   Levels 18-31: 20 XP + 3 per level above 17
+//   Levels 32+:   62 XP + 7 per level above 31
 
-//  #####################################
-//  ###                               ###
-//  ### MyPet Exp-System Level Script ###
-//  ###         by Keyle              ###
-//  ###       MC 1.3.1 script         ###
-//  #####################################
-//
-//      Usable Fields for the "info" Object:
-//          info.type
-//          info.worldGroup
-//
-
-//  Level 2-16 cost 17 XP points each
-//  Level 17-31 cost 3 more XP points than the previous
-//  Level 32-∞ cost 7 more XP points than the previous
-
-//   |------------------|
-//   |  Return Methods  |
-//   |------------------|
+// Pre-calculated tier boundaries:
+var EXP_AT_17 = 272;   // (17-1) * 17
+var EXP_AT_32 = 887;   // EXP_AT_17 + sum of costs for levels 18-31, plus level 32's base
 
 function getExpByLevel(level, petType, worldGroup) {
     if (level <= 1) {
         return 0;
     }
-    var exp = 0, i;
-    if (level > 31) {
-        exp = 887;
-        level -= 31;
-        for (i = 1; i < level; i++) {
-            exp += 62 + (i * 7);
-        }
-        return exp;
+    if (level <= 17) {
+        // Flat 17 XP per level
+        return (level - 1) * 17;
     }
-    if (level > 17) {
-        exp = 272;
-        level -= 17;
-        for (i = 1; i <= level; i++) {
-            exp += 17 + (i * 3);
-        }
-        return exp;
+    if (level <= 31) {
+        // Arithmetic series: costs are 20, 23, 26, ... (increment of 3)
+        var n = level - 17;
+        return EXP_AT_17 + n * (3 * level - 14) / 2;
     }
-    return (level - 1) * 17;
+    // Level 32+: costs are 69, 76, 83, ... (increment of 7)
+    var n = level - 32;
+    return EXP_AT_32 + n * (7 * level - 93) / 2;
 }
