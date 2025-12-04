47ab694f3789a60b416b1b2c1b057bc511306016
UserDerezzed <me@userderezzed.dev>
2025-12-04 00:09:02 -0800

    Fix Javascript exp accumulation

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,6 +135,7 @@ tasks.processResources {
 
 fun Manifest.attributesForMyPet() = attributes(
     mapOf(
+        "Class-Path" to "MyPet/rhino.jar MyPet/rhino-1.7.9.jar MyPet/rhino-1.7.10.jar MyPet/rhino-1.7.15.jar ../MyPet/rhino.jar ../MyPet/rhino-1.7.9.jar ../MyPet/rhino-1.7.10.jar ../MyPet/rhino-1.7.15.jar MyPet/mongo-java-driver.jar MyPet/mongo-java-driver-3.12.11.jar",
         "Main-Class" to "de.Keyle.MyPet.Main",
         "Project-Name" to project.name,
         "Project-Version" to project.version.toString(),
