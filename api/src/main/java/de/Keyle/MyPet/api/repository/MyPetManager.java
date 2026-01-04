7aee245edbd7fe015a5bf8203b42ad0fdbfcd374
UserDerezzed <me@userderezzed.dev>
2026-01-05 02:39:42 +0800

    (MySQL) Fix potential "connection leak detected" warning when a player disconnects

--- api/src/main/java/de/Keyle/MyPet/api/repository/MyPetManager.java
+++ api/src/main/java/de/Keyle/MyPet/api/repository/MyPetManager.java
@@ -79,10 +79,6 @@ public abstract class MyPetManager {
     public abstract Optional<MyPet> activateMyPet(StoredMyPet storedMyPet);
 
     public boolean deactivateMyPet(MyPetPlayer owner, boolean update) {
-        return deactivateMyPet(owner, update, false);
-    }
-
-    public boolean deactivateMyPet(MyPetPlayer owner, boolean update, boolean save) {
         if (mActivePlayerPets.containsKey(owner)) {
             final MyPet myPet = owner.getMyPet();
 
@@ -92,9 +88,6 @@ public abstract class MyPetManager {
             myPet.removePet();
             if (update) {
                 MyPetApi.getRepository().updateMyPet(myPet, null);
-            }
-            if (save) {
-                MyPetApi.getRepository().savePet(myPet);
             }
             mActivePetsPlayer.remove(myPet);
             return true;
