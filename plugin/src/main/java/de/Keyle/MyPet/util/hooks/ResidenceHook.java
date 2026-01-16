ccfcf18ee247bb81691d9b747f75677008d61b0e
UserDerezzed <me@userderezzed.dev>
2026-01-15 21:57:52 -0700

    Fix Pets being able to enter a Residence or activate pressure plates when their owner can't
    Fixes #1383

--- plugin/src/main/java/de/Keyle/MyPet/util/hooks/ResidenceHook.java
+++ plugin/src/main/java/de/Keyle/MyPet/util/hooks/ResidenceHook.java
@@ -24,18 +24,25 @@ import com.bekvon.bukkit.residence.Residence;
 import com.bekvon.bukkit.residence.containers.Flags;
 import com.bekvon.bukkit.residence.protection.FlagPermissions;
 import de.Keyle.MyPet.MyPetApi;
+import de.Keyle.MyPet.api.entity.MyPetBukkitEntity;
 import de.Keyle.MyPet.api.player.MyPetPlayer;
 import de.Keyle.MyPet.api.util.hooks.PluginHookName;
+import de.Keyle.MyPet.api.util.hooks.types.AllowedHook;
 import de.Keyle.MyPet.api.util.hooks.types.FlyHook;
 import de.Keyle.MyPet.api.util.hooks.types.MountInsideHook;
 import de.Keyle.MyPet.api.util.hooks.types.PlayerVersusEntityHook;
 import de.Keyle.MyPet.api.util.hooks.types.PlayerVersusPlayerHook;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.entity.EntityInteractEvent;
 
 @PluginHookName("Residence")
-public class ResidenceHook implements PlayerVersusPlayerHook, PlayerVersusEntityHook, FlyHook, MountInsideHook {
+public class ResidenceHook implements PlayerVersusPlayerHook, PlayerVersusEntityHook, FlyHook, MountInsideHook, AllowedHook {
 
     Residence residence;
 
@@ -46,9 +53,18 @@ public class ResidenceHook implements PlayerVersusPlayerHook, PlayerVersusEntity
 
             FlagPermissions.addFlag("mypet-fly");
             FlagPermissions.addFlag("mypet-damage");
+
+            // Register events for pressure plate and area entry handling
+            Bukkit.getPluginManager().registerEvents(this, MyPetApi.getPlugin());
+
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onDisable() {
+        HandlerList.unregisterAll(this);
     }
 
     @Override
@@ -89,5 +105,45 @@ public class ResidenceHook implements PlayerVersusPlayerHook, PlayerVersusEntity
         } catch (Throwable ignored) {
         }
         return false;
+    }
+
+    @Override
+    public boolean isPetAllowed(MyPetPlayer player) {
+        try {
+            Player p = player.getPlayer();
+            FlagPermissions flagPermissions = residence.getPermsByLoc(p.getLocation());
+            // Check if player has "move" permission in this residence
+            return flagPermissions.playerHas(p, Flags.move, true);
+        } catch (Throwable ignored) {
+        }
+        return true;
+    }
+
+    @EventHandler
+    public void on(EntityInteractEvent event) {
+        Entity ent = event.getEntity();
+        if (ent instanceof MyPetBukkitEntity) {
+            Block block = event.getBlock();
+            String blockTypeName = block.getType().name();
+
+            // Check all pressure plate variants (modern + legacy names)
+            if (blockTypeName.contains("PRESSURE_PLATE") ||
+                blockTypeName.equals("WOOD_PLATE") ||
+                blockTypeName.equals("STONE_PLATE") ||
+                blockTypeName.equals("IRON_PLATE") ||
+                blockTypeName.equals("GOLD_PLATE")) {
+
+                try {
+                    Player owner = ((MyPetBukkitEntity) ent).getOwner().getPlayer();
+                    FlagPermissions flagPermissions = residence.getPermsByLoc(block.getLocation());
+
+                    // Check "use" flag for the player (pressure plates fall under "use" permission)
+                    if (!flagPermissions.playerHas(owner, Flags.use, true)) {
+                        event.setCancelled(true);
+                    }
+                } catch (Throwable ignored) {
+                }
+            }
+        }
     }
 }
