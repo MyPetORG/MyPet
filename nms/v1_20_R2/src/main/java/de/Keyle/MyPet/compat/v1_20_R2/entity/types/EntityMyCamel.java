231c464c578a764683ce9cfac3e148ed004977fa
UserDerezzed <me@userderezzed.dev>
2026-03-16 20:00:43 +0800

    Remove dead catch(Error) blocks from EntityMyCamel hurtAndBreak calls

--- nms/v1_20_R2/src/main/java/de/Keyle/MyPet/compat/v1_20_R2/entity/types/EntityMyCamel.java
+++ nms/v1_20_R2/src/main/java/de/Keyle/MyPet/compat/v1_20_R2/entity/types/EntityMyCamel.java
@@ -24,8 +24,6 @@ import de.Keyle.MyPet.api.Configuration;
 import de.Keyle.MyPet.api.entity.EntitySize;
 import de.Keyle.MyPet.api.entity.MyPet;
 import de.Keyle.MyPet.api.entity.types.MyCamel;
-import de.Keyle.MyPet.api.util.ErrorUtil;
-import de.Keyle.MyPet.compat.v1_20_R2.CompatManager;
 import de.Keyle.MyPet.compat.v1_20_R2.entity.EntityMyPet;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
@@ -41,8 +39,6 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import org.bukkit.Sound;
 import org.bukkit.craftbukkit.v1_20_R2.inventory.CraftItemStack;
-
-import java.lang.reflect.InvocationTargetException;
 
 @EntitySize(width = 0.9F, height = 1.7F)
 public class EntityMyCamel extends EntityMyPet {
@@ -120,18 +116,7 @@ public class EntityMyCamel extends EntityMyPet {
                 getBukkitEntity().getWorld().playSound(getBukkitEntity().getLocation(), org.bukkit.Sound.ENTITY_SHEEP_SHEAR, 1.0F, 1.0F);
                 getMyPet().setSaddle(null);
                 if (itemStack != ItemStack.EMPTY && !entityhuman.getAbilities().instabuild) {
-                    try {
-                        itemStack.hurtAndBreak(1, entityhuman, (entityhuman1) -> entityhuman1.broadcastBreakEvent(enumhand));
-                    } catch (Error e) {
-                        // TODO REMOVE
-                        itemStack.hurtAndBreak(1, entityhuman, (entityhuman1) -> {
-                            try {
-                                CompatManager.ENTITY_LIVING_broadcastItemBreak.invoke(entityhuman1, enumhand);
-                            } catch (IllegalAccessException | InvocationTargetException ex) {
-                                ErrorUtil.report(ex);
-                            }
-                        });
-                    }
+                    itemStack.hurtAndBreak(1, entityhuman, (entityhuman1) -> entityhuman1.broadcastBreakEvent(enumhand));
                 }
 
                 return InteractionResult.CONSUME;
