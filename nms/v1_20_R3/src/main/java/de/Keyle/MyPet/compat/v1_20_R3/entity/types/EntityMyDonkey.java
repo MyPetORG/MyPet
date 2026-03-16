982acf5486e59290763624c202135d9b91b54eac
UserDerezzed <me@userderezzed.dev>
2026-03-16 20:01:23 +0800

    Remove dead catch(Error) blocks from EntityMyDonkey hurtAndBreak calls

--- nms/v1_20_R3/src/main/java/de/Keyle/MyPet/compat/v1_20_R3/entity/types/EntityMyDonkey.java
+++ nms/v1_20_R3/src/main/java/de/Keyle/MyPet/compat/v1_20_R3/entity/types/EntityMyDonkey.java
@@ -25,7 +25,6 @@ import de.Keyle.MyPet.api.entity.EntitySize;
 import de.Keyle.MyPet.api.entity.MyPet;
 import de.Keyle.MyPet.api.entity.types.MyDonkey;
 import de.Keyle.MyPet.api.util.ErrorUtil;
-import de.Keyle.MyPet.compat.v1_20_R3.CompatManager;
 import de.Keyle.MyPet.compat.v1_20_R3.entity.EntityMyPet;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -47,7 +46,6 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.bukkit.Sound;
 import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftItemStack;
 
-import java.lang.reflect.InvocationTargetException;
 
 @EntitySize(width = 1.4F, height = 1.6F)
 public class EntityMyDonkey extends EntityMyPet {
@@ -141,18 +139,7 @@ public class EntityMyDonkey extends EntityMyPet {
                     getMyPet().setChest(null);
                     getMyPet().setSaddle(null);
                     if (itemStack != ItemStack.EMPTY && !entityhuman.getAbilities().instabuild) {
-                        try {
-                            itemStack.hurtAndBreak(1, entityhuman, (entityhuman1) -> entityhuman1.broadcastBreakEvent(enumhand));
-                        } catch (Error e) {
-                            // TODO REMOVE
-                            itemStack.hurtAndBreak(1, entityhuman, (entityhuman1) -> {
-                                try {
-                                    CompatManager.ENTITY_LIVING_broadcastItemBreak.invoke(entityhuman1, enumhand);
-                                } catch (IllegalAccessException | InvocationTargetException ex) {
-                                    ErrorUtil.report(ex);
-                                }
-                            });
-                        }
+                        itemStack.hurtAndBreak(1, entityhuman, (entityhuman1) -> entityhuman1.broadcastBreakEvent(enumhand));
                     }
                 }
 
