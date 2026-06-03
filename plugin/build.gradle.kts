7d6bbd0a9670583240d4f47e4bb31c6fba9b7a0b
UserDerezzed <me@userderezzed.dev>
2026-06-03 04:30:16 -0700

    Fix compiling

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -55,7 +55,9 @@ dependencies {
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         isTransitive = false
     }
-    compileOnly("com.gmail.nossr50.mcMMO:mcMMO:2.2.052-SNAPSHOT")
+    compileOnly("com.gmail.nossr50.mcMMO:mcMMO:2.2.052-SNAPSHOT") {
+        isTransitive = false // don't drag in worldguard-core 7.0.12 etc.
+    }
 
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.7")
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.20")
@@ -66,10 +68,17 @@ dependencies {
     compileOnly("net.slipcor:pvparena:1.3.4.298")
     compileOnly("me.maker56.survivalgames:SurvivalGames:1.9.4")
     compileOnly("org.mcsg:survivalgames:0.6.7")
-    compileOnly("com.github.Zrips:Residence:6.0.0.1")
+    compileOnly("com.github.Zrips:Residence:6.0.0.1") {
+        isTransitive = false // don't drag in org.dynmap:dynmap-api (no public host)
+    }
     compileOnly("com.palmergames.bukkit.towny:towny:0.103.0.2")
     compileOnly("net.sacredlabyrinth.phaed.simpleclans:SimpleClans:2.19.2")
-    compileOnly("io.github.fabiozumbi12.RedProtect:RedProtect-Spigot:8.1.2")
+    compileOnly("io.github.fabiozumbi12.RedProtect:RedProtect-Spigot:8.1.2") {
+        isTransitive = false // don't drag in spigot-api/UltimateFancy; Core added explicitly below
+    }
+    compileOnly("io.github.fabiozumbi12.RedProtect:RedProtect-Core:8.1.2") {
+        isTransitive = false
+    }
     compileOnly("com.github.TechFortress:GriefPrevention:16.18")
     compileOnly("me.NoChance.PvPManager:pvpmanager:3.19.10")
     compileOnly("com.github.cryptomorin:kingdoms:1.17.18.1-BETA") {
@@ -81,7 +90,7 @@ dependencies {
     compileOnly("com.kirelcodes.miniaturepets:miniaturepets-api:1.5.4")
     compileOnly("com.github.sirblobman.combatlogx:api:11.6-SNAPSHOT")
     compileOnly("com.github.sirblobman.api:core:2.8-SNAPSHOT") // BlueSlimeCore (Java 21-compatible; 2.9-SNAPSHOT is Java 25)
-    compileOnly("uk.antiperson.stackmob:StackMob:5.10.6")
+    compileOnly("uk.antiperson.stackmob:StackMob:5.10.3") // 5.10.4+ are built for Java 25; 5.10.3 is Java 17
     compileOnly("me.glaremasters:guilds:3.5.7.0")
     compileOnly("com.github.Angeschossen:LandsAPI:4.5.2.0")
     compileOnly("net.kyori:adventure-text-minimessage:4.17.0")
