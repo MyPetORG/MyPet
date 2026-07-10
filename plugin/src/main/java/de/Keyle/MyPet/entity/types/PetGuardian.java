93a5e8df14514bf1362e1eef4424a44da70ba13c
UserDerezzed <me@userderezzed.dev>
2026-07-10 13:19:05 -0700

    Fix guardian pets damaging their own owner

--- plugin/src/main/java/de/Keyle/MyPet/entity/types/PetGuardian.java
+++ plugin/src/main/java/de/Keyle/MyPet/entity/types/PetGuardian.java
@@ -22,11 +22,20 @@ package de.Keyle.MyPet.entity.types;
 
 import de.Keyle.MyPet.api.config.ConfigKey;
 import de.Keyle.MyPet.api.entity.DefaultInfo;
+import de.Keyle.MyPet.api.entity.Pet;
 import de.Keyle.MyPet.api.entity.PetAquaticEntity;
 import de.Keyle.MyPet.api.entity.ShopInfo;
+import de.Keyle.MyPet.api.listener.PetListenerRegistry;
 import de.Keyle.MyPet.api.player.MyPetPlayer;
 import de.Keyle.MyPet.entity.PetImpl;
+import de.Keyle.MyPet.listeners.PetListenerGuards;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
+import java.util.function.Supplier;
 
 @ShopInfo
 @DefaultInfo(food = {Material.SUGAR}, flySpeed = 1.1013D)
@@ -35,8 +44,24 @@ public class PetGuardian extends PetImpl implements PetAquaticEntity {
     public static final ConfigKey<Boolean> CAN_SWIM = ConfigKey.bool("Guardian", "CanSwim", true);
     public static final ConfigKey<Boolean> PREVENT_SUFFOCATION = ConfigKey.bool("Guardian", "PreventSuffocation", true);
 
+    public static final Supplier<Listener> SPIKE_OWNER_PROTECTOR =
+            PetListenerRegistry.register(SpikeOwnerProtector::new);
 
     public PetGuardian(MyPetPlayer petOwner) {
         super(petOwner);
+    }
+
+    /** Stops a Guardian pet's vanilla spikes (thorns) from hurting its own owner. */
+    public static final class SpikeOwnerProtector implements Listener {
+
+        @EventHandler(ignoreCancelled = true)
+        public void onGuardianSpikeDamage(EntityDamageByEntityEvent event) {
+            if (event.getCause() != DamageCause.THORNS) return;
+            Pet pet = PetListenerGuards.markedPet(event.getDamager()).orElse(null);
+            if (!(pet instanceof PetGuardian)) return;
+            if (pet.getOwner().equals(event.getEntity())) {
+                event.setCancelled(true);
+            }
+        }
     }
 }
